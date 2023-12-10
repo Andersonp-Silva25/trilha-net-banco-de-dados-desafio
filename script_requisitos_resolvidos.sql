2537744c7c4b3bd65791be16bbaa3845f3a831a0
Andersonp-Silva25 <andersonp.silva25@gmail.com>
2023-12-09 22:45:39 -0300

    :sparkles:Feat: Requisito 3

--- script_requisitos_resolvidos.sql
+++ script_requisitos_resolvidos.sql
@@ -3,3 +3,6 @@ SELECT Nome, Ano FROM Filmes
 
 -- Requisito 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT * FROM Filmes ORDER BY Ano
+
+-- Requisito 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT * FROM Filmes WHERE Nome = 'De Volta para o Futuro'
