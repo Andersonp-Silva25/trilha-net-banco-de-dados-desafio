9d261553592c63d125b63a1be355a2c445f8f570
Andersonp-Silva25 <andersonp.silva25@gmail.com>
2023-12-09 22:42:48 -0300

    :sparkles:Feat: Requisito 2

---
+++ script_requisitos_resolvidos.sql
@@ -0,0 +1,5 @@
+-- Requisito 1 - Buscar o nome e ano dos filmes
+SELECT Nome, Ano FROM Filmes
+
+-- Requisito 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT * FROM Filmes ORDER BY Ano
